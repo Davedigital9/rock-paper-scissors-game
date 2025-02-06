89da310c186e1baf860a6cdac0be2ca16599782a
Davedigital <agbooladavid001@outlook.com>
2025-02-06 17:44:48 +0000

    Added the function to view the rules of the game and improved styling

--- script.js
+++ script.js
@@ -80,3 +80,33 @@ const playAgain = () => {           // function to play again
     let contest = document.querySelector(".contest");
     contest.style.display = "none";
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM fully loaded and parsed');
+
+    // Get the modal
+    const modal = document.getElementById('rules-modal');
+
+    // Get the button that opens the modal
+    const btn = document.getElementById('show-rules-btn');
+
+    // Get the <span> element that closes the modal
+    const span = document.getElementsByClassName('close')[0];
+
+    // When the user clicks the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = 'block';
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = 'none';
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+        }
+    }
+});
